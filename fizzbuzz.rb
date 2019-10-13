3044403d4b18a7c999de025bd639b60b014d05ff
Gary Schwaeber <gschwaeb@gmail.com>
2019-10-13 21:09:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(int)
   int.to_i
-  if ((int % 3 == 0) && (int % 5 == 0))
-    puts "FizzBuzz"
-  elsif (int % 3 == 0)
+  if (int % 3 == 0)
     puts "Fizz"
   elsif (int % 5 == 0)
     puts "Buzz"
-  else
+  elsif ((int % 3 == 0) && (int % 5 == 0))
+    puts "FizzBuzz"
+      else
     puts "nil"
     end
   end
